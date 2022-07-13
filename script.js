3b2c8469b62c15ffbc02b76b70e566e9853492c5
Gustavo-Aquino-1 <guhaquino10@gmail.com>
2022-07-13 18:55:23 -0300

    requisitos 17,18,19

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 const senha = document.getElementsByClassName('senha')[0];
 const email = document.getElementsByClassName('email')[0];
 const btnEntrar = document.querySelector('.btnEntrar');
+const checkValid = document.querySelector('#agreement');
+const submit = document.getElementById('submit-btn');
+
+function validaCheck() {
+  if (checkValid.checked === true) {
+    submit.removeAttribute('disabled');
+  } else {
+    submit.setAttribute('disabled', 'disabled');
+  }
+}
 
 function validarEntrada() {
   if (email.value === 'tryber@teste.com' && senha.value === '123456') {
@@ -13,3 +23,4 @@ function validarEntrada() {
 // chamadas
 
 btnEntrar.addEventListener('click', validarEntrada);
+checkValid.addEventListener('click', validaCheck);
